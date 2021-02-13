baac34e9886ed09d273b754b337290f2cc34a6a7
Naveen <navi.mehta@gmail.com>
2021-02-14 10:56:10 +1100

    pagination component updates

---
+++ src/Components/Pagination/Pagination.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import Pagination from '@material-ui/lab/Pagination';
+import { Context } from '../../Context/Context'
+
+const Paginate = () => {
+    const { empData, profilesPerPage, activePage, setActivePage } = useContext(Context);
+    
+    // calculate total pages for the pagination component to display page count
+    const pageCount = Math.ceil(empData.length / profilesPerPage)
+
+    // update active page in context, fires when user selects a page
+    const updateActivePage = (pageNum) => setActivePage(pageNum);
+
+    return (
+        <Pagination 
+            onChange={(event, val) => {updateActivePage(val)}}
+            count={pageCount}
+            page={activePage} 
+        />
+    );
+};
+
+export default Paginate;
